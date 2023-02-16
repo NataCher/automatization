a577611c2218526cb0d31cc25c9bcf106d7b4611
dyalik <98204933+NataCher@users.noreply.github.com>
2023-02-16 22:01:32 +0300

    Add files via upload

---
+++ house_nata/main.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+import sys
+
+ui, _= loadUiType('Livingroom.ui')
+ui, _= loadUiType('Bathroom.ui')
+ui, _= loadUiType('Bedroom.ui')
+ui, _= loadUiType('Garage.ui')
+ui, _= loadUiType('Kitchen.ui')
+ui, _= loadUiType('Menu.ui')
+
+
+
+
+
+
